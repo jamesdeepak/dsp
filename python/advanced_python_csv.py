8585c70b7b4358c34b277ea09ac5f1d23120c3f1
jamesdeepak <deepakgautam123@gmail.com>
2016-06-18 12:13:43 -0400

    Update Advanced Python csv
    @sampathweb

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,21 @@
-PLACE YOUR CODE HERE
+import csv
+
+with open('faculty.csv', newline='') as csvfile:
+    myreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    next(myreader,None) #ignoring the header
+
+    allEmailList = []
+
+    for row in myreader:
+
+        #get email addresses
+        email = row[3].strip()
+        if email != "":
+            allEmailList.append(email)
+
+
+csvfile = "emails.csv"
+with open(csvfile, "w") as output:
+    writer = csv.writer(output, lineterminator='\n')
+    for val in allEmailList:
+        writer.writerow([val])
